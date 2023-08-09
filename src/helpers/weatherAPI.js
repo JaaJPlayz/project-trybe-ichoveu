53904d48d5455134f360d1f807a1c73b162b6bf5
= <=>
2023-08-09 10:37:31 -0300

    Finished the 1st task

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,20 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const TOKEN = import.meta.env.VITE_TOKEN;
+
+export const searchCities = async (term) => {
+  const url = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+  try {
+    const response = await fetch(url);
+    const resolveData = await response.json();
+    console.log(resolveData);
+    if (resolveData.length === 0) {
+      alert('Nenhuma cidade encontrada');
+    }
+    return resolveData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
